1b87878fa77007c87de4eff21b262f860819b2d1
Daridjcm <daricorreamedina@gmail.com>
2024-07-06 15:22:39 -0500

    Fix deployment failed for use APIs browser

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,19 +1,21 @@
-document.addEventListener('mousemove', (e) => {
-    const h2 = document.querySelector('h2');
-    const rect = h2.getBoundingClientRect();
-    const x = e.clientX - rect.left - rect.width / 2;
-    const y = e.clientY - rect.top - rect.height / 2;
+if (typeof window !== 'undefined') {
+    document.addEventListener('mousemove', (e) => {
+        const h2 = document.querySelector('h2');
+        const rect = h2.getBoundingClientRect();
+        const x = e.clientX - rect.left - rect.width / 2;
+        const y = e.clientY - rect.top - rect.height / 2;
 
-    h2.style.transform = `translate(${x * 0.02}px, ${y * 0.02}px)`;
+        h2.style.transform = `translate(${x * 0.02}px, ${y * 0.02}px)`;
 
-    if (
-        e.clientX >= rect.left &&
-        e.clientX <= rect.right &&
-        e.clientY >= rect.top &&
-        e.clientY <= rect.bottom
-    ) {
-        h2.classList.add('hover');
-    } else {
-        h2.classList.remove('hover');
-    }
-});
+        if (
+            e.clientX >= rect.left &&
+            e.clientX <= rect.right &&
+            e.clientY >= rect.top &&
+            e.clientY <= rect.bottom
+        ) {
+            h2.classList.add('hover');
+        } else {
+            h2.classList.remove('hover');
+        }
+    });
+}
